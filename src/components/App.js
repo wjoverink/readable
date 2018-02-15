f6aca24aeac436d35c6605cf94961562ee24c559
wjoverink <willemjan.overink@gmail.com>
2018-02-15 14:49:32 +0100

    chore: setting up base of project

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import Button from 'material-ui/Button';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+
+      </div>
+    );
+  }
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,54 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import 'typeface-roboto'
+import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
+import { createMuiTheme, MuiThemeProvider } from 'material-ui/styles';
+import Reboot from 'material-ui/Reboot';
+import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
+import reducer from './reducers';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      light: '#8bf6ff',
+      main: '#4fc3f7',
+      dark: '#0093c4',
+      contrastText: '#000',
+    },
+    secondary: {
+      light: '#ff8a50',
+      main: '#ff5722',
+      dark: '#c41c00',
+      contrastText: '#fff',
+    },
+  },
+});
+
+// const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+//
+// const store = createStore(
+//   reducer,
+//   composeEnhancers(
+//     applyMiddleware(logger)
+//   )
+// )
+
+function AppTheme() {
+  return (
+    // <Provider store={store}>
+      <MuiThemeProvider theme={theme}>
+        <Reboot />
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </MuiThemeProvider>
+    // </Provider>
+  );
+}
+
+ReactDOM.render(<AppTheme />, document.getElementById('root'));
 registerServiceWorker();
